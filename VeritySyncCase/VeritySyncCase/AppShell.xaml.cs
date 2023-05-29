d39d2ccbdd70bc79e6c3afff33e864d3139930f5
phi.lekim <phik.le@mission.plus>
2023-05-29 22:31:04 +0700

    initial project

---
+++ VeritySyncCase/VeritySyncCase/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VeritySyncCase;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ VeritySyncCase/VeritySyncCase/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VeritySyncCase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NCC")]
+[assembly: AssemblyProduct("VeritySyncCase")]
+[assembly: AssemblyCopyright("Copyright © NCC 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
