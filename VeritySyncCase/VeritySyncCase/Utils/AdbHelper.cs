e59095fd6c35c12414e76b329822a0808a81a45f
phi.lekim <phik.le@mission.plus>
2023-05-31 18:17:36 +0700

    update main logic

--- VeritySyncCase/VeritySyncCase/Utils/AdbHelper.cs
+++ VeritySyncCase/VeritySyncCase/Utils/AdbHelper.cs
@@ -4,13 +4,39 @@ using Windows.Storage;
 
 namespace VeritySyncCase.Utils
 {
+
+    public static class FileHelper
+    {
+        public static string ReadTextFile(string filePath)
+        {
+            // Read the contents of the file
+            string fileContent = File.ReadAllText(filePath);
+            return fileContent;
+        }
+        public static void CreateFolder(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error create folder: " + ex.Message);
+            }
+        }
+    }
+
     public static class AdbHelper
     {
-        public static string GetFilePathInDocuments(string fileName, string deviceName)
+        public static string GetFilePathInDocuments(string destinationFolderPath, string deviceId)
         {
+            var sourcePath = "/sdcard/Documents";
             // Set the ADB command to list the file path in the documents folder on the specific device
-            //string adbCommand = $"-s {deviceName} shell find /sdcard/Documents -name {fileName}";
-            string adbCommand = $"shell find /sdcard/Documents -name {fileName}";
+            string adbCommand = $"-s {deviceId} pull {sourcePath} {destinationFolderPath}";
             // Create a new process start info for executing ADB
             ProcessStartInfo processStartInfo = new ProcessStartInfo
             {
@@ -32,43 +58,7 @@ namespace VeritySyncCase.Utils
             // Wait for the process to exit
             process.WaitForExit();
 
-            // Remove any line breaks or whitespace characters from the output
-            output = output.Trim();
-
-            // Check if the file path exists
-            if (string.IsNullOrEmpty(output))
-            {
-                throw new FileNotFoundException($"File '{fileName}' not found in the documents folder on the device '{deviceName}'.");
-            }
-
             return output;
-        }
-
-        public static void CopyFileToDevice(string sourceFilePath, string destinationFolderPath)
-        {
-            try
-            {
-                if (!File.Exists(sourceFilePath))
-                {
-                    Console.WriteLine("Source file does not exist.");
-                    return;
-                }
-
-                if (!Directory.Exists(destinationFolderPath))
-                {
-                    Directory.CreateDirectory(destinationFolderPath);
-                }
-
-                string fileName = Path.GetFileName(sourceFilePath);
-
-                string destinationFilePath = Path.Combine(destinationFolderPath, fileName);
-
-                File.Copy(sourceFilePath, destinationFilePath, true);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error copying file: " + ex.Message);
-            }
         }
     }
 }
